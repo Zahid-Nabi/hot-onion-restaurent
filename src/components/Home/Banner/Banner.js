9e0925df0f917ac69f09b24cc23f8361885632c5
Zahid-Nabi <jobaidunnabi@gmail.com>
2021-10-14 00:24:25 +0600

    banner section added

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { FormControl, InputGroup, Button, Container } from 'react-bootstrap';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            <h2>Banner Section</h2>
+        <div className="banner">
+            <Container className="text-container">
+                <h1>Best food waiting for your belly</h1>
+                <InputGroup className="my-3">
+                    <FormControl
+                        placeholder="Search Food Items"
+                        aria-label="Search Food Items"
+                        aria-describedby="basic-addon2"
+                    />
+                    <Button className="search-btn" variant="danger" id="button-addon2">
+                        Search
+                    </Button>
+                </InputGroup>
+            </Container>
         </div>
     );
 };
